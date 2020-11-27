9b6ba0035a9a036b81c5866352d35e88e31b9b9c
Denis <freenedix@gmail.com>
2020-11-27 19:11:29 +0300

    Use xxd for trim lines

--- wsconv.sh
+++ wsconv.sh
@@ -3,7 +3,7 @@
 # This script is designed to search from the exported wireshark POST objects and convert it to a format convenient for viewing.
 # Work tested on Linux and BSD systems and exclude specefic platform tools (tail -r on BSD, tac on Linux etc)
 # tags: wireshark, export objects, post, mime types, wireshark export images, wireshark post upload.
-# by n0a
+# by n0a 2020 | https://n0a.pw
 
 # Output directory
 dir=found_files 
@@ -22,8 +22,11 @@ if [ -w $pwd ]; then
 			# In $foundname var we get filename from wireshark objects file.
 			foundname="$(sed -n 2p $file | awk '{print $4}' | cut -c10- | sed -e 's/\"//g' | tr -d '\r')"
 			echo "[+] Convert $file to $foundname"
-			# Delete first and last lines with info from wireshark
-	        [ ! -f "$dir/$foundname" ] && cat "$file" | tail -n +5 | sed -e "$(($(wc -l < $file | awk '{ print $1 }')-8)),\$d" > "$dir/$foundname"
+			# Delete first and last lines from wireshark objects with xxd without damage
+	        [ ! -f "$dir/$foundname" ] && xxd -p "$file" | tr -d '\n' | \
+										  sed -e 's/0d0a0d0a/z/1' -e 's/^.*z//1' \
+										      -e 's/0d0a2d2d2d2d2d2d5765624b6974466f726d426f756e64617279.*$//1' | \
+										  xxd -p -r > "$dir/$foundname"
 		done
 	    echo "[!] Success. Convert $(ls $dir | wc -l | awk '{ print $1 }') from $(ls $pwd | wc -l | awk '{ print $1 }') files."
 	    echo "[*] Listing $dir:"
